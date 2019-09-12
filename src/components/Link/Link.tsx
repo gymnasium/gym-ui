12a9b0c5b31da1dfd3d162b9359d57bec300770d
Ali Hamoody <alih@dev6.com>
2019-09-12 16:08:46 -0400

    Added dark mode for link

--- src/components/Link/Link.tsx
+++ src/components/Link/Link.tsx
@@ -1,24 +1,44 @@
 /** @jsx jsx */
 import { jsx, css } from '@emotion/core';
-import { color } from '../../colors';
+import { color, typeface } from '../../colors';
 // import classes from './Link.module.css';
 
-const style = css`
+const styleLight = css`
   text-decoration: none;
   color: ${color.orange};
+  font-family: ${typeface.text};
+  transition: all 0.1s linear 0s;
   :hover {
     text-decoration: none;
     border-bottom: 1px solid;
   }
 `;
 
+const styleDark = css`
+  text-decoration: none;
+  color: ${color.grey[800]};
+  background-color: transparent;
+  font-family: ${typeface.title};
+  transition: all 0.1s linear 0s;
+  :hover {
+    text-decoration: none;
+    border: 0px;
+    color: ${color.orange};
+  }
+`;
+
 interface LinkProps {
   children: React.ReactChild;
   to?: string;
+  dark?: boolean;
 }
 
-const Link = ({ children, to = '#' }: LinkProps): React.ReactElement => (
-  <a css={style} href={to}>
+const Link = ({
+  children,
+  to = '#',
+  dark = false,
+}: LinkProps): React.ReactElement => (
+  <a css={dark ? styleDark : styleLight} href={to}>
     {children}
   </a>
 );
--- stories/Link.stories.tsx
+++ stories/Link.stories.tsx
@@ -3,8 +3,28 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 
 import { Link } from '../src';
+import { color } from '../src/colors';
 
-storiesOf('Link', module).addWithJSX(
-  'default',
-  (): React.ReactElement => <Link>This is a link</Link>
-);
+storiesOf('Link', module)
+  .addWithJSX(
+    'default (light)',
+    (): React.ReactElement => (
+      <>
+        <Link>This is a link on a light background</Link>
+        <div style={{ backgroundColor: color.black }}>
+          <Link>This is a link on a dark background</Link>
+        </div>
+      </>
+    )
+  )
+  .addWithJSX(
+    'dark',
+    (): React.ReactElement => (
+      <>
+        <div style={{ backgroundColor: color.black }}>
+          <Link dark>This is a dark link on a dark background</Link>
+        </div>
+        <Link dark>This is a dark link on a light background</Link>
+      </>
+    )
+  );
