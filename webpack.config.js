6734fc008ef45a4124f6a021034d8922208df0e3
Mike Bifulco <mbifulco@live.com>
2019-09-12 10:12:37 -0400

    try a slightly different approach…

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
-import path from 'path';
+// import path from 'path';
 
-var ExtractTextPlugin = require('extract-text-webpack-plugin');
+// const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
   mode: 'production',
@@ -36,15 +36,10 @@ module.exports = {
         test: /\.css$/,
         // include: path.join(__dirname, 'src'),
         use: [
-          'style-loader',
-          {
-            loader: 'typings-for-css-modules-loader',
-            options: {
-              modules: true,
-              namedExport: true,
-              camelCase: true,
-            },
-          },
+          { loader: 'style-loader' }, // to inject the result into the DOM as a style block
+          { loader: 'css-modules-typescript-loader' }, // to generate a .d.ts module next to the .scss file (also requires a declaration.d.ts with "declare modules '*.scss';" in it to tell TypeScript that "import styles from './styles.scss';" means to load the module "./styles.scss.d.td")
+          { loader: 'css-loader', options: { modules: true } }, // to convert the resulting CSS to Javascript to be bundled (modules:true to rename CSS classes in output to cryptic identifiers, except if wrapped in a :global(...) pseudo class)
+          // { loader: 'sass-loader' }, // to convert SASS to CSS
         ],
       },
       // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
