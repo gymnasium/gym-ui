af5623b800983e2df8217da1dfa95fbc8aed48a0
Ali Hamoody <alih@dev6.com>
2019-09-16 10:48:49 -0400

    Added subtitle component

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,6 @@ import Button from './components/Button';
 import Link from './components/Link';
 import Card from './components/Card';
 import Title from './components/Title';
+import Subtitle from './components/Subtitle';
 
-export { Button, Link, Card, Title };
+export { Button, Link, Card, Title, Subtitle };
