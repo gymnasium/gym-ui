34d863815d7d835145aeed3a612ceea2bb9bd69a
Ali Hamoody <alih@dev6.com>
2019-09-16 16:01:49 -0400

    Adding styles to bulleted list WIP

--- src/components/BulletedList/BulletedList.tsx
+++ src/components/BulletedList/BulletedList.tsx
@@ -2,10 +2,33 @@
 import { jsx, css } from '@emotion/core';
 import { color, typeface } from '../../styles';
 
-const listStyle = css``;
-const titleStyle = css``;
-const dataStyle = css``;
-const dataSpanStyle = css``;
+const style = css`
+  font: 1em/1.2 ${typeface.title};
+  color: ${color.black};
+  letter-spacing: 0.06em;
+  word-spacing: 0.05em;
+  margin-bottom: 0.6em;
+  margin-left: 1.2em;
+  margin-top: 1em;
+
+  > dt {
+    font: 900 1em/1.2 ${typeface.title};
+    text-transform: uppercase;
+    margin-bottom: 0.6em;
+  }
+
+  > dd {
+    display: list-item;
+    list-style: disc;
+    color: ${color.orange};
+    margin-top: 0.3em;
+
+    > span {
+      font: 1em/1.4 ${typeface.text};
+      color: ${color.black};
+    }
+  }
+`;
 
 interface Props {
   title?: string;
@@ -17,14 +40,14 @@ const BulletedList: React.FunctionComponent<Props> = ({
   data,
 }: Props): React.ReactElement => {
   return (
-    <dl css={listStyle}>
-      {title && <dt css={titleStyle}>{title}</dt>}
+    <dl css={style}>
+      {title && <dt>{title}</dt>}
       {data &&
         data.map(
           (dataItem): React.ReactElement => {
             return (
-              <dd css={dataStyle} key={dataItem}>
-                <span css={dataSpanStyle}>{dataItem}</span>
+              <dd key={dataItem}>
+                <span>{dataItem}</span>
               </dd>
             );
           }
