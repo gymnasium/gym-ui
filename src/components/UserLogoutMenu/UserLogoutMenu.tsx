f5e2c31129bd0a2ff96065f705f6cb1c7c156b24
Ali Hamoody <alih@dev6.com>
2019-09-17 16:14:28 -0400

    User logout menu temp commit WIP

--- src/components/UserLogoutMenu/UserLogoutMenu.tsx
+++ src/components/UserLogoutMenu/UserLogoutMenu.tsx
@@ -1,8 +1,37 @@
 /** @jsx jsx */
 import { jsx, css } from '@emotion/core';
+import { useState, Fragment } from 'react';
 import { color, typeface } from '../../styles';
 
-const style = css``;
+const style = css`
+  font: normal 1.2em/1 ${typeface.title};
+  display: inline-block;
+  color: ${color.orange};
+  border: none;
+`;
+const caretDownStyle = css`
+  display: inline-block;
+  width: 0;
+  height: 0;
+  margin-left: 0.25em;
+  vertical-align: middle;
+  border-top: 0.25em solid;
+  border-right: 0.25em solid transparent;
+  border-left: 0.25em solid transparent;
+`;
+const caretRightStyle = css`
+  display: inline-block;
+  width: 0;
+  height: 0;
+  margin-left: 0.25em;
+  line-height: 0;
+  padding: 0 0.2em;
+  vertical-align: middle;
+  border-top: 0.25em solid transparent;
+  border-left: 0.25em solid;
+  border-bottom: 0.25em solid transparent;
+  /* margin-left: -0.8em; */
+`;
 
 interface Props {
   userId: string;
@@ -10,9 +39,27 @@ interface Props {
 
 const UserLogoutMenu: React.FunctionComponent<Props> = ({
   userId,
-}: Props): React.ReactElement => (
-  <p>
-    Logout <b>{userId}</b>
-  </p>
-);
+}: Props): React.ReactElement => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <Fragment>
+      <span css={style}>
+        <span>{userId}</span>
+        <span css={caretRightStyle} />
+        <a href="/logout" role="menuitem">
+          Log Out
+        </a>
+      </span>
+      <br />
+      <span css={style}>
+        <span>{userId}</span>
+        <span css={caretDownStyle} />
+        <a href="/logout" role="menuitem">
+          Log Out
+        </a>
+      </span>
+    </Fragment>
+  );
+};
 export default UserLogoutMenu;
