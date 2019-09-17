1a8836acb84bca2c7a29b7e530580caf721ae1f4
Ali Hamoody <alih@dev6.com>
2019-09-17 14:57:43 -0400

    TabbedContainer color overrides and additional story

--- src/components/TabbedContainer/TabbedContainer.tsx
+++ src/components/TabbedContainer/TabbedContainer.tsx
@@ -3,67 +3,74 @@ import { jsx, css } from '@emotion/core';
 import { color, typeface } from '../../styles';
 // import { H1 } from '../Headings';
 
-const style = css`
-  border: none;
-  background: transparent;
-  padding-top: 0;
-  padding-bottom: 4em;
-  margin: 0 auto 0;
-
-  :nth-of-type(odd) {
-    color: ${color.grey.lightPageBackground};
-    background-color: ${color.grey.dark};
-    > header > h1::before {
-      border-bottom: 1.4em solid ${color.grey.dark};
-    }
-  }
-  :nth-of-type(even) {
-    color: ${color.grey.dark};
-    background-color: ${color.grey.lightPageBackground};
-    > header > h1::before {
-      border-bottom: 1.4em solid ${color.grey.lightPageBackground};
-    }
-  }
-
-  header {
-    text-align: center;
-    h1 {
-      display: inline-block;
-      font-size: 1.75em;
-      line-height: 1.2;
-      text-transform: uppercase;
-      vertical-align: 1.2em;
-      margin-top: -1em;
-      text-align: center;
-
-      ::before {
-        display: inline-block;
-        width: 100%;
-        height: 0;
-        content: '';
-        line-height: 0;
-        padding-right: 1em;
-        padding-left: 1em;
-        border-right: 1.4em dashed transparent;
-        border-left: 1.4em dashed transparent;
-        box-sizing: border-box;
-        vertical-align: top;
-        position: relative;
-        z-index: 100;
-      }
-    }
-  }
-`;
-
 interface Props {
   title?: string;
   children: React.ReactNode;
+  textColor?: string;
+  backgroundColor?: string;
 }
 
 const TabbedContainer: React.FunctionComponent<Props> = ({
   title = '',
   children,
+  textColor = '',
+  backgroundColor = '',
 }: Props): React.ReactElement => {
+  const style = css`
+    border: none;
+    background: transparent;
+    padding-top: 0;
+    padding-bottom: 4em;
+    margin: 0 auto 0;
+    font-family: ${typeface.text};
+
+    :nth-of-type(odd) {
+      color: ${textColor || color.grey.lightPageBackground};
+      background-color: ${backgroundColor || color.grey.dark};
+      > header > h1::before {
+        border-bottom: 1.4em solid ${backgroundColor || color.grey.dark};
+      }
+    }
+    :nth-of-type(even) {
+      color: ${textColor || color.grey.dark};
+      background-color: ${backgroundColor || color.grey.lightPageBackground};
+      > header > h1::before {
+        border-bottom: 1.4em solid
+          ${backgroundColor || color.grey.lightPageBackground};
+      }
+    }
+
+    header {
+      text-align: center;
+      font-family: ${typeface.title};
+      h1 {
+        display: inline-block;
+        font-size: 1.75em;
+        line-height: 1.2;
+        text-transform: uppercase;
+        vertical-align: 1.2em;
+        margin-top: -1em;
+        text-align: center;
+
+        ::before {
+          display: inline-block;
+          width: 100%;
+          height: 0;
+          content: '';
+          line-height: 0;
+          padding-right: 1em;
+          padding-left: 1em;
+          border-right: 1.4em dashed transparent;
+          border-left: 1.4em dashed transparent;
+          box-sizing: border-box;
+          vertical-align: top;
+          position: relative;
+          z-index: 100;
+        }
+      }
+    }
+  `;
+
   return (
     <div css={style}>
       {title && (
--- stories/TabbedContainer.stories.tsx
+++ stories/TabbedContainer.stories.tsx
@@ -5,126 +5,70 @@ import { storiesOf } from '@storybook/react';
 import { TabbedContainer } from '../src';
 import { color } from '../src/styles';
 
-storiesOf('TabbedContainer', module).addWithJSX(
-  'with title',
-  (): React.ReactElement => {
-    const title = 'How Gymnasium Works';
-    return (
-      <>
-        <div style={{ backgroundColor: color.green }}>
-          <br />
-          Content Before
-          <br />
-          <br />
-        </div>
-        <TabbedContainer title="Tab 1">111</TabbedContainer>
-        <TabbedContainer title="Tab 2">222</TabbedContainer>
-
-        <TabbedContainer title={`${title} - Tab 3`}>
-          <section className="row how-gymnasium-works">
-            <div className="col-md-3 col-sm-6 col-xs-12">
-              <h2>Free Online Courses</h2>
-              <p>
-                Our courses are taught by industry experts. You can enroll
-                anytime, anywhere.
-              </p>
-            </div>
-          </section>
-
-          <div className="row">
-            <section className="highlighted-courses col-md-12">
-              <header className="text-center header-tab">
-                <h1>Featured Courses</h1>
-              </header>
-
-              <section className="courses row">
-                <div className="section-featured-courses">
-                  <div className="cols">
-                    <ul className="list-featured-courses">
-                      <li>
-                        <div className="card featured-course user-experience">
-                          <div className="card-main equal-h">
-                            <header>
-                              <b className="course-type">User Experience</b>
-                              <div className="mask">
-                                <a
-                                  href="https://thegymnasium.com/courses/course-v1:GYM+015+0/about"
-                                  title="Learn More"
-                                >
-                                  <img
-                                    alt="Prototyping for Digital Products and Websites"
-                                    srcSet="https://gymcms.xyz/img/course-artwork/svg/gym-015.svg"
-                                    src="https://thegymnasium.com/asset-v1:GYM+015+0+type@asset+block@gym-015.png"
-                                    width="256"
-                                  />
-                                </a>
-                              </div>
-                              <h2>
-                                <a
-                                  href="https://thegymnasium.com/courses/course-v1:GYM+015+0/about"
-                                  title="Learn More"
-                                >
-                                  Prototyping for Digital Products and Websites
-                                </a>
-                              </h2>
-                              <p>
-                                A case for using prototyping in your design
-                                process to boost efficient collaboration.
-                              </p>
-                            </header>
-                          </div>
-                          <div className="card-footer">
-                            <div className="card-info">
-                              <dl className="instructor">
-                                <dt className="byline">
-                                  <b>with Jenna Bantjes</b>
-                                </dt>
-                                <dd>Experience Designer</dd>
-                              </dl>
-                            </div>
-                            <div className="card-cta">
-                              <a
-                                className="gym-button"
-                                href="https://thegymnasium.com/courses/course-v1:GYM+015+0/about"
-                              >
-                                <b>Learn More</b>
-                              </a>
-                            </div>
-                          </div>
-                        </div>
-                      </li>
-                    </ul>
-                  </div>
-                  <div className="about-featured-courses">
-                    <div className="description">
-                      <p>
-                        Gymnasium offers free online courses on web development,
-                        design, user experience, and content creation.
-                      </p>
-                    </div>
-                    <div className="cta">
-                      <a
-                        className="gym-button"
-                        href="https://thegymnasium.com/courses"
-                      >
-                        <b>View all Courses</b>
-                      </a>
-                    </div>
-                  </div>
-                </div>
-              </section>
-            </section>
+storiesOf('TabbedContainer', module)
+  .addWithJSX(
+    'default colors',
+    (): React.ReactElement => {
+      const title = 'How Gymnasium Works';
+      return (
+        <>
+          <div style={{ backgroundColor: color.green }}>
+            <br />
+            Content Before
+            <br />
+            <br />
           </div>
-        </TabbedContainer>
-        <TabbedContainer title="Tab 4">444</TabbedContainer>
+          <TabbedContainer title="Tab 1">111</TabbedContainer>
+          <TabbedContainer title="Tab 2">222</TabbedContainer>
 
-        <div style={{ backgroundColor: color.purple }}>
-          <br />
-          Content After
-          <br />
-          <br />
-        </div>
-      </>
-    );
-  }
-);
+          <TabbedContainer title={title}>
+            <h2>Free Online Courses</h2>
+            <p>
+              Our courses are taught by industry experts. You can enroll
+              anytime, anywhere.
+            </p>
+          </TabbedContainer>
+          <TabbedContainer title="Tab 4">444</TabbedContainer>
+          <div style={{ backgroundColor: color.purple }}>
+            <br />
+            Content After
+            <br />
+            <br />
+          </div>
+        </>
+      );
+    }
+  )
+  .addWithJSX(
+    'color override',
+    (): React.ReactElement => {
+      const title = 'How Gymnasium Works';
+      return (
+        <>
+          <div style={{ backgroundColor: color.green }}>
+            <br />
+            Content Before
+            <br />
+            <br />
+          </div>
+          <TabbedContainer
+            title={title}
+            backgroundColor={color.black}
+            textColor={color.orange}
+          >
+            <h2>Free Online Courses</h2>
+            <p>
+              Our courses are taught by industry experts. You can enroll
+              anytime, anywhere.
+            </p>
+          </TabbedContainer>
+          <div style={{ backgroundColor: color.purple }}>
+            <br />
+            Content After
+            <br />
+            <br />
+          </div>
+        </>
+      );
+    }
+  );
