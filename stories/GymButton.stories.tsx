a29998a94ffb15fac7927edc55a28bad976b551a
Mike Bifulco <mbifulco@aquent.com>
2019-09-18 10:28:49 -0400

    use actions addon for button stories

--- stories/GymButton.stories.tsx
+++ stories/GymButton.stories.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import { storiesOf } from '@storybook/react';
+import { action } from '@storybook/addon-actions';
 
 import { GymButton } from '../src';
 import { color } from '../src/styles';
@@ -9,22 +10,24 @@ storiesOf('GymButton', module)
   .addWithJSX(
     'default',
     (): React.ReactElement => (
-      <GymButton
-        onClick={(): void => {
-          alert('Clicked!');
-        }}
-      >
-        Default
-      </GymButton>
+      <GymButton onClick={action('Button clicked')}>Default</GymButton>
     )
   )
   .addWithJSX(
     'full width',
-    (): React.ReactElement => <GymButton fullWidth>Full Width</GymButton>
+    (): React.ReactElement => (
+      <GymButton fullWidth onClick={action('Button clicked')}>
+        Full Width
+      </GymButton>
+    )
   )
   .addWithJSX(
     'all caps',
-    (): React.ReactElement => <GymButton allCaps>all caps</GymButton>
+    (): React.ReactElement => (
+      <GymButton allCaps onClick={action('Button clicked')}>
+        all caps
+      </GymButton>
+    )
   )
   .addWithJSX(
     'light border on a dark background',
@@ -35,7 +38,9 @@ storiesOf('GymButton', module)
           padding: '10px',
         }}
       >
-        <GymButton lightBorder>Light Border</GymButton>
+        <GymButton lightBorder onClick={action('Button clicked')}>
+          Light Border
+        </GymButton>
       </div>
     )
   );
