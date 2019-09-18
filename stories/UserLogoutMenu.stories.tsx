ec74c7604bf28fafa7f25f4a79be8814fc91ab5f
Mike Bifulco <mbifulco@aquent.com>
2019-09-18 16:44:30 -0400

    add background to logoutmenu story

--- stories/UserLogoutMenu.stories.tsx
+++ stories/UserLogoutMenu.stories.tsx
@@ -3,8 +3,21 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 
 import { UserLogoutMenu } from '../src';
+import { color } from '../src/styles';
 
-storiesOf('UserLogoutMenu', module).addWithJSX(
-  'default',
-  (): React.ReactElement => <UserLogoutMenu userId="Ali" logoutUrl="/logout" />
-);
+storiesOf('UserLogoutMenu', module)
+  .addParameters({
+    backgrounds: [{ name: 'Nav black', value: color.grey[900], default: true }],
+  })
+  .addWithJSX(
+    'default',
+    (): React.ReactElement => (
+      <div
+        style={{
+          backgroundColor: color.black,
+        }}
+      >
+        <UserLogoutMenu userId="Ali" logoutUrl="/logout" />
+      </div>
+    )
+  );
