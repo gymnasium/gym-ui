cd552f45eaec4ad80f86800fc75e319a464eb94a
Ali Hamoody <alih@dev6.com>
2019-09-23 10:50:31 -0400

    Added GymLogo component

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import Button from './components/Button';
+import GymLogo from './components/GymLogo';
 import Link from './components/Link';
 import Card from './components/Card';
 import BulletedList from './components/BulletedList';
@@ -9,6 +10,7 @@ import { H1, H2, H3, H4 } from './components/Headings';
 
 export {
   Button,
+  GymLogo,
   Link,
   Card,
   BulletedList,
---
+++ stories/GymLogo.stories.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import { storiesOf } from '@storybook/react';
+
+import { GymLogo } from '../src';
+import { color } from '../src/styles';
+
+storiesOf('GymLogo', module)
+  .addParameters({
+    component: GymLogo,
+  })
+  .addWithJSX('default', (): React.ReactElement => <GymLogo />)
+  .addWithJSX(
+    'with dimensions override',
+    (): React.ReactElement => <GymLogo width="300px" height="50px" />
+  )
+  .addWithJSX(
+    'background color override',
+    (): React.ReactElement => <GymLogo backgroundColor={color.black} />
+  );
