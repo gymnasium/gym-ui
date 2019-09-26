dc7de061a6ec6e914dff3c05eb1ce39455657725
Ali Hamoody <alih@dev6.com>
2019-09-26 11:38:01 -0400

    Basic list completed, job listing WIP

--- src/components/List/List.tsx
+++ src/components/List/List.tsx
@@ -3,34 +3,69 @@ import { jsx, css } from '@emotion/core';
 import { color, typeface } from '../../styles';
 
 const style = css`
-  font: 1em/1.2 ${typeface.title};
+  border: 1px red solid;
+  font: 700 0.8rem/1.3 ${typeface.title};
   color: ${color.black};
-  letter-spacing: 0.06em;
-  word-spacing: 0.05em;
-  margin-bottom: 0.6em;
-  margin-left: 1.2em;
-  margin-top: 1em;
+  border-bottom: 1px dashed ${color.grey.regular};
+  /* padding: 1rem; */
+  /* letter-spacing: 0.06em; */
+  /* word-spacing: 0.05em; */
+  /* margin-bottom: 1rem;
+  margin-left: 1rem;
+  margin-top: 1rem; */
+
+  ul {
+    padding: 0rem;
+    margin: 0rem;
+    list-style: none;
+  }
+  li {
+    border-bottom: 1px solid ${color.grey.regular};
+    padding: 1rem 1rem;
+    font-weight: normal;
+  }
+  li:last-of-type {
+    border-bottom: none;
+  }
+  header {
+    padding-top: 0.75rem;
+  }
+  footer {
+    border-top: 1px solid ${color.grey.regular};
+    padding: 1rem 0;
+  }
 `;
-const titleStyle = css`
-  font: 900 1em/1.2 ${typeface.title};
-  text-transform: uppercase;
-  margin-bottom: 0.6em;
-`;
+// const titleStyle = css`
+//   font: 900 1em/1.2 ${typeface.title};
+//   text-transform: uppercase;
+//   margin-bottom: 0.6em;
+// `;
 
 interface Props {
-  title?: string;
   children: React.ReactNode;
+  /** List header content string or markup */
+  headerContent?: React.ReactNode;
+  /** List footer content string or markup */
+  footerContent?: React.ReactNode;
 }
 
 const List: React.FunctionComponent<Props> = ({
-  title = '',
   children,
+  headerContent,
+  footerContent,
 }: Props): React.ReactElement => {
   return (
-    <ul css={style}>
-      {title && <div css={titleStyle}>{title}</div>}
-      {children}
-    </ul>
+    // <ul css={style}>
+    //   {title && <div css={titleStyle}>{title}</div>}
+    //   {children}
+    // </ul>
+    <article css={style}>
+      {headerContent && <header>{headerContent}</header>}
+      <section>
+        <ul>{children}</ul>
+      </section>
+      {footerContent && <footer>{footerContent}</footer>}
+    </article>
   );
 };
 
--- stories/List.stories.tsx
+++ stories/List.stories.tsx
@@ -2,22 +2,77 @@ import React from 'react';
 
 import { storiesOf } from '@storybook/react';
 
-import { List, ListItem } from '../src';
+import { List, ListItem, Link } from '../src';
 
 storiesOf('List', module)
   .addParameters({
     component: List,
   })
   .addWithJSX(
-    'with title',
+    'Simple with header and footer',
     (): React.ReactElement => {
-      const title = 'Skills covered';
+      const headerContent = 'Title goes here';
+      const footerContent = 'List ends at this footer here';
       return (
-        <List title={title}>
-          <ListItem>HTML basics</ListItem>
-          <ListItem>CSS basics</ListItem>
-          <ListItem>Bootstrap basics</ListItem>
-          <ListItem>And more...</ListItem>
+        <List headerContent={headerContent} footerContent={footerContent}>
+          <ListItem>Simple item, text only</ListItem>
+          <ListItem>
+            Can also contain <b>markup</b>
+          </ListItem>
+          <ListItem>
+            Or even a <Link to="/">Link</Link>
+          </ListItem>
+        </List>
+      );
+    }
+  )
+  .addWithJSX(
+    'Job listing items',
+    (): React.ReactElement => {
+      const headerContent = (
+        <>
+          Viewing jobs in <em>Charlotte</em>...
+        </>
+      );
+      const footerContent = (
+        <Link to="http://aquent.com/find-work/?l=61&amp;utm_source=gymnasium&amp;utm_medium=web&amp;utm_campaign=homepagejobs&amp;utm_content=viewall">
+          View all jobs in <em>Charlotte</em> →
+        </Link>
+      );
+      return (
+        // <List title={title}>
+        //   <ListItem>HTML basics</ListItem>
+        //   <ListItem>CSS basics</ListItem>
+        //   <ListItem>Bootstrap basics</ListItem>
+        //   <ListItem>And more...</ListItem>
+        // </List>
+        <List headerContent={headerContent} footerContent={footerContent}>
+          <ListItem class="row gym-microservice-job-listing">
+            <a
+              href="https://aquent.com/find-work/151781?utm_source=thegymnasium.com&amp;utm_medium=web&amp;utm_campaign=job-module#content"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <div class="job-post">
+                <b class="job-title col-xs-8">Project Manager</b>{' '}
+                <em class="job-market col-xs-4 text-right">Charlotte, NC </em>
+              </div>
+            </a>
+          </ListItem>
+          <ListItem class="row gym-microservice-job-listing">
+            <a
+              href="https://aquent.com/find-work/151702?utm_source=thegymnasium.com&amp;utm_medium=web&amp;utm_campaign=job-module#content"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <div class="job-post">
+                <b class="job-title col-xs-8">
+                  Digital Marketing Product Manager
+                </b>{' '}
+                <em class="job-market col-xs-4 text-right">Charlotte, NC</em>
+              </div>
+            </a>
+          </ListItem>
         </List>
       );
     }
